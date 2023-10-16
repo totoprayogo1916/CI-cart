097d028066f52c654155424d7bd5bc894ad9ba4b
totoprayogo1916 <totoprayogo1916@gmail.com>
2023-10-16 23:28:55 +0700

    separate Config

---
+++ src/Publishers/ConfigPublisher.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Totoprayogo1916\CodeIgniter\Publishers;
+
+use CodeIgniter\Publisher\Publisher;
+
+class ConfigPublisher extends Publisher
+{
+    /**
+     * Tell Publisher where to get the files.
+     * Since we will use Composer to download
+     * them we point to the "vendor" directory.
+     *
+     * @var string
+     */
+    protected $source = VENDORPATH . 'totoprayogo1916/codeigniter4-cart/src/Config/';
+
+    /**
+     * FCPATH is always the default destination,
+     * but we may want them to go in a sub-folder
+     * to keep things organized.
+     *
+     * @var string
+     */
+    protected $destination = APPPATH . 'Config';
+
+    /**
+     * Use the "publish" method to indicate that this
+     * class is ready to be discovered and automated.
+     */
+    public function publish(): bool
+    {
+        return $this
+            ->addPath('Cart.php')
+            ->copy();
+    }
+}
