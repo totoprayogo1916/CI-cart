a0a70fc462e0469e6561f54b05a24f124cb56a1c
Toto <2387514+totoprayogo1916@users.noreply.github.com>
2023-11-28 21:41:39 +0700

    fix return

--- src/Helpers/cart_helper.php
+++ src/Helpers/cart_helper.php
@@ -4,7 +4,7 @@ if (! function_exists('cart')) {
     /**
      * Provides a convenience interface to the Cart service.
      *
-     * @return void
+     * @return void|object|array
      */
     function cart()
     {
